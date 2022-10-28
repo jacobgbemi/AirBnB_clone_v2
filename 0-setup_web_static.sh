d17e2a3f4570708164f549e9d77e0e7fbf9d4014
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-10-28 11:20:53 +0000

    task 0 debugged

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,55 @@
 #!/usr/bin/env bash
-# Sets up webservers for deployment: (Run script on both servers)
-# If not done, does the following:
-#     installs Nginx; creates folders /data/, /data/web_static/,
-#     /data/web_static/releases/, /data/web_static/shared,
-#     /data/web_static/releases/test
-#     /data/web_static/releases/test/index.html (with some content)
-# Create symbolic link /data/web_static/current to data/web_static/releases/test
-#     delete and recreate symbolic link each time script's ran
-# Recursively assign ownership of /data/ folder to user and group 'ubuntu'
-# Update the Nginx config to serve content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static)
-#     restart Nginx
-# curl localhost/hbnb_static/index.html should return sample text"
+# Script to set up the web servers for the deployment of web_static
 
-ADD_WEBSTATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+echo -e "\e[1;32m START\e[0m"
 
-sudo apt-get update
+#--Updating the packages
+sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Packages updated\e[0m"
+echo
+
+#--configuring firewall
+sudo ufw allow 'Nginx HTTP'
+echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
+echo
+
+#--creating the directrry
+sudo mkdir -p /data/web_static/releases/test/data/web_static/shared
+echo -e "\e[1;32m directories created"
+echo
+
+#--adding test string
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Test string added\e[0m"
+echo
+
+#--prevent overwrite
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+fi;
+echo -e "\e[1;32m prevent overwrite\e[0m"
+echo
+
+#--create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "35i $ADD_WEBSTATIC" /etc/nginx/sites-available/default
-sudo service nginx start
+sudo chown -hR ubuntu:ubuntu /data
+
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+echo -e "\e[1;32m Symbolic link created\e[0m"
+echo
+
+#--restart NGINX
+sudo service nginx restart
+echo -e "\e[1;32m restart NGINX\e[0m"
