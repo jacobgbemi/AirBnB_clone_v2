36cb4404c09877e9c64e3cdaa0bf0be7686d0a5e
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-11-02 18:48:06 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,55 +1,41 @@
 #!/usr/bin/env bash
-# Script to set up the web servers for the deployment of web_static
+# This scripts sets up my web servers for the deployment of web_static
 
-echo -e "\e[1;32m START\e[0m"
-
-#--Updating the packages
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-echo -e "\e[1;32m Packages updated\e[0m"
-echo
 
-#--configuring firewall
-sudo ufw allow 'Nginx HTTP'
-echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
-echo
-
-#--creating the directrry
-sudo mkdir -p /data/web_static/releases/test/data/web_static/shared
-echo -e "\e[1;32m directories created"
-echo
-
-#--adding test string
-echo "<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo '<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-echo -e "\e[1;32m Test string added\e[0m"
-echo
-
-#--prevent overwrite
-if [ -d "/data/web_static/current" ];
-then
-    echo "path /data/web_static/current exists"
-    sudo rm -rf /data/web_static/current;
-fi;
-echo -e "\e[1;32m prevent overwrite\e[0m"
-echo
-
-#--create symbolic link
+</html>' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data
-
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-echo -e "\e[1;32m Symbolic link created\e[0m"
-echo
-
-#--restart NGINX
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://google.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default
 sudo service nginx restart
-echo -e "\e[1;32m restart NGINX\e[0m"
